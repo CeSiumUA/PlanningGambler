d44f307788095732c3270eb15af6c8308b8e0449
Fedir Katushonok <fedir.katushonok@gmail.com>
2022-02-11 22:26:39 +0200

    Added hubconnector

--- PlanningGambler.Front/Program.cs
+++ PlanningGambler.Front/Program.cs
@@ -12,6 +12,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7049") });
 builder.Services.AddScoped<IRoomConnectionProvider, RoomConnectionService>();
+builder.Services.AddScoped<HubConnectionService>();
 builder.Services.AddMatBlazor();
 builder.Services.AddBlazoredLocalStorage();
 
---
+++ PlanningGambler.Front/Services/Concrete/HubConnectionService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace PlanningGambler.Front.Services.Concrete
+{
+    public class HubConnectionService : IAsyncDisposable
+    {
+        private readonly HubConnection _hubConnection;
+
+        private string? _token;
+
+        public HubConnectionService()
+        {
+            this._hubConnection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:7049/planninghub", options =>
+                {
+                    options.AccessTokenProvider = async () => await this.RetreiveToken();
+                })
+                .Build();
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await this._hubConnection?.StopAsync();
+            await this._hubConnection.DisposeAsync();
+        }
+
+        public async Task StartConnection(string token)
+        {
+            this._token = token;
+            await this._hubConnection.StartAsync();
+        }
+
+        private async Task<string> RetreiveToken()
+        {
+            return this._token;
+        }
+    }
+}
