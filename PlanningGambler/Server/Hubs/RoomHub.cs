a7964fa2f7b80d5254fd9056b357b1bc033b43e7
Fedir Katushonok <mtg.in.ua@gmail.com>
2022-11-20 11:09:04 +0200

    Added authentication

---
+++ PlanningGambler/Server/Hubs/RoomHub.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace PlanningGambler.Server.Hubs;
+
+public class RoomHub : Hub
+{
+}
--- PlanningGambler/Server/ServiceExtensions.cs
+++ PlanningGambler/Server/ServiceExtensions.cs
@@ -1,9 +1,13 @@
 ﻿using MediatR;
 using MediatR.Extensions.FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using PlanningGambler.Server.Commands;
 using PlanningGambler.Server.Models;
 using PlanningGambler.Server.Services;
 using PlanningGambler.Server.Services.Interfaces;
+using System.Diagnostics;
 
 namespace PlanningGambler.Server;
 
@@ -13,11 +17,55 @@ public static class ServiceExtensions
     {
         var assembly = typeof(CreateRoomCommand).GetType().Assembly;
 
+        services.AddSignalR();
+
+        var tokenService = new TokenKeyService();
+
         return services
             .AddMediatR(typeof(Room).Assembly)
             .AddSingleton<IRoomStorage, RoomStorageService>()
-            .AddSingleton<ITokenKeyProvider, TokenKeyService>()
+            .AddSingleton<ITokenKeyProvider>(tokenService)
+            .AddConfiguredAuthentication(tokenService)
             .AddMediatR(assembly)
-            .AddFluentValidation(new[] { assembly });
+            .AddFluentValidation(new[] { assembly })
+            .AddAuthorization();
+    }
+
+    public static IServiceCollection AddConfiguredAuthentication(this IServiceCollection services, ITokenKeyProvider tokenKeyProvider)
+    {
+        services
+            .AddAuthentication(auth =>
+            {
+                auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = true;
+                options.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var accessToken = context.Request.Query["access_token"];
+                        var path = context.HttpContext.Request.Path;
+                        if (!string.IsNullOrEmpty(accessToken) &&
+                            path.StartsWithSegments("/roomhub"))
+                            context.Token = accessToken;
+                        return Task.CompletedTask;
+                    }
+                };
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidIssuer = tokenKeyProvider.GetIssuer(),
+                    ValidateIssuer = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidateAudience = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(tokenKeyProvider.GetKey())
+                };
+            });
+
+        return services;
     }
 }
