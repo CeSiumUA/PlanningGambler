3463306c3d33ea90cd4f8d388b4316036e09ef14
Fedir Katushonok <fedir.katushonok@gmail.com>
2022-11-20 17:23:18 +0200

    Added basic layout

--- PlanningGambler/Client/Program.cs
+++ PlanningGambler/Client/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using PlanningGambler.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services
+    .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+    .AddMudServices();
 
 await builder.Build().RunAsync();
