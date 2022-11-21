921ab775baa7f8de93a938fce8cc81ddc9f01df9
Fedir Katushonok <fedir.katushonok@gmail.com>
2022-11-21 22:01:58 +0200

    temp

--- PlanningGambler/Client/Models/AppConstants.cs
+++ PlanningGambler/Client/Models/AppConstants.cs
@@ -1,6 +1,24 @@
-﻿namespace PlanningGambler.Client.Models;
+﻿using PlanningGambler.Shared.Data;
+
+namespace PlanningGambler.Client.Models;
 
 public class AppConstants
 {
     public const string TokenStoreKey = "stored_token";
+
+    public static Dictionary<VoteType, string> VoteSelectValues { get; } = new Dictionary<VoteType, string>()
+    {
+        {VoteType.None, "None" },
+        {VoteType.One, "1" },
+        {VoteType.Two, "2" },
+        {VoteType.Three, "3" },
+        {VoteType.Five, "5" },
+        {VoteType.Eight, "8" },
+        {VoteType.Thirteen, "13" },
+        {VoteType.TwentyOne, "21" },
+        {VoteType.ThirtyFour, "34" },
+        {VoteType.FiftyFive, "55" },
+        {VoteType.EightyNine, "89" },
+        {VoteType.HoldOn, "Hold" }
+    };
 }
--- PlanningGambler/Client/Services/InterComponentsService.cs
+++ PlanningGambler/Client/Services/InterComponentsService.cs
@@ -1,11 +1,37 @@
-﻿namespace PlanningGambler.Client.Services;
+﻿using PlanningGambler.Client.Models;
+using PlanningGambler.Shared.Data;
+
+namespace PlanningGambler.Client.Services;
 
 public class InterComponentsService
 {
     public event EventHandler? ShareRoomEventTriggered;
 
+    public event EventHandler<VoteType>? VoteStated;
+
+    public event EventHandler<string>? SelectedStageChanged;
+
+    public event GetRoomStagesModels? GetRoomStagesModelsRequested; 
+
+    public delegate RoomModel GetRoomStagesModels();
+
     public void ShareRoom()
     {
         ShareRoomEventTriggered?.Invoke(this, default!);
+    }
+
+    public void Vote(VoteType voteType)
+    {
+        VoteStated?.Invoke(this, voteType);
+    }
+
+    public void SetSelectedStageName(string stageName)
+    {
+        SelectedStageChanged?.Invoke(this, stageName);
+    }
+
+    public RoomModel? GetStages()
+    {
+        return GetRoomStagesModelsRequested?.Invoke();
     }
 }
